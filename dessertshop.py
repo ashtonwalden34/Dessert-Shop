1ce32257daa0ac8557513480b655f51a0693a514
Ashton Walden <ashtonwalden@icloud.com>
2022-10-22 13:19:26 -0600

    added input functions

---
+++ dessertshop.py
@@ -0,0 +1,14 @@
+def main_menu():
+    pass
+
+def user_prompt_candy():
+    pass
+
+def user_prompt_cookies():
+    pass
+
+def user_prompt_icecream():
+    pass
+
+def user_prompt_sundae():
+    pass
